96eb237967c674f525da67d3d9d48403d166323d
Abner Benavides <abnerb86@gmail.com>
2014-12-20 14:54:47 -0800

    tutorial

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get  'about' => 'static_page#about'
   get  'contact' => 'static_page#contact'
   get 'signup' => 'users#new'
+
   resources :microposts
 
   resources :users
